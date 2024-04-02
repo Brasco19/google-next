836c833a86e78a2b83ce64261ce4b9278dcd3800
iambrasco <ebangabdon641@gmail.com>
2024-04-02 23:16:39 +0900

    fix last section

--- src/components/ImageSearchResults.jsx
+++ src/components/ImageSearchResults.jsx
@@ -6,7 +6,7 @@ function ImageSearchResults({results}) {
   return (
     <div className='sm:pb-24 pb-40 mt-4 '>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4">
-        {results.items?.map((result)=>(
+        {results.items.map((result)=>(
           <div className="mb-8" key={result.link}>
             <div className="group">
               <Link href={result.image.contextLink}>
--- src/components/PaginationButton.jsx
+++ src/components/PaginationButton.jsx
@@ -1,4 +1,4 @@
-'use cleint'
+'use client'
 
 import React from 'react'
 import { usePathname, useSearchParams } from 'next/navigation.js'
